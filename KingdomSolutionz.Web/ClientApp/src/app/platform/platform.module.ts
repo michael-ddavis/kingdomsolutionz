0049a65d418c4e5ad9db4a63da0736e89fc9c951
michael-davis <47865749+michael-ddavis@users.noreply.github.com>
2026-08-04 15:15:37 -0400

    Register Engagements travel workspace

--- KingdomSolutionz.Web/ClientApp/src/app/platform/platform.module.ts
+++ KingdomSolutionz.Web/ClientApp/src/app/platform/platform.module.ts
@@ -2,58 +2,25 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import {
-  WorkspaceSwitcherComponent
-} from './components/workspace-switcher/workspace-switcher.component';
-import {
-  PlatformShellComponent
-} from './layout/platform-shell/platform-shell.component';
-import {
-  AssignmentActivityComponent
-} from './pages/assignments/assignment-activity/assignment-activity.component';
-import {
-  AssignmentContactsComponent
-} from './pages/assignments/assignment-contacts/assignment-contacts.component';
-import {
-  AssignmentCareNetworkComponent
-} from './pages/assignments/assignment-care-network/assignment-care-network.component';
-import {
-  AssignmentDetailComponent
-} from './pages/assignments/assignment-detail/assignment-detail.component';
-import {
-  AssignmentDocumentsComponent
-} from './pages/assignments/assignment-documents/assignment-documents.component';
-import {
-  AssignmentListComponent
-} from './pages/assignments/assignment-list/assignment-list.component';
-import {
-  AssignmentOverviewComponent
-} from './pages/assignments/assignment-overview/assignment-overview.component';
-import {
-  AssignmentWorkspaceComponent
-} from './pages/assignments/assignment-workspace/assignment-workspace.component';
-import {
-  CareNetworkInboxComponent
-} from './pages/care-network/care-network-inbox.component';
-import {
-  PlatformDashboardComponent
-} from './pages/platform-dashboard/platform-dashboard.component';
-import {
-  PlatformLoginComponent
-} from './pages/platform-login/platform-login.component';
-import {
-  PartnerReferralResponseComponent
-} from './pages/referrals/partner-referral-response/partner-referral-response.component';
-import {
-  SpeakingRequestDetailComponent
-} from './pages/speaking-requests/speaking-request-detail/speaking-request-detail.component';
-import {
-  SpeakingRequestListComponent
-} from './pages/speaking-requests/speaking-request-list/speaking-request-list.component';
+import { WorkspaceSwitcherComponent } from './components/workspace-switcher/workspace-switcher.component';
+import { PlatformShellComponent } from './layout/platform-shell/platform-shell.component';
+import { AssignmentActivityComponent } from './pages/assignments/assignment-activity/assignment-activity.component';
+import { AssignmentContactsComponent } from './pages/assignments/assignment-contacts/assignment-contacts.component';
+import { AssignmentCareNetworkComponent } from './pages/assignments/assignment-care-network/assignment-care-network.component';
+import { AssignmentDetailComponent } from './pages/assignments/assignment-detail/assignment-detail.component';
+import { AssignmentDocumentsComponent } from './pages/assignments/assignment-documents/assignment-documents.component';
+import { AssignmentListComponent } from './pages/assignments/assignment-list/assignment-list.component';
+import { AssignmentOverviewComponent } from './pages/assignments/assignment-overview/assignment-overview.component';
+import { AssignmentTravelComponent } from './pages/assignments/assignment-travel/assignment-travel.component';
+import { AssignmentWorkspaceComponent } from './pages/assignments/assignment-workspace/assignment-workspace.component';
+import { CareNetworkInboxComponent } from './pages/care-network/care-network-inbox.component';
+import { PlatformDashboardComponent } from './pages/platform-dashboard/platform-dashboard.component';
+import { PlatformLoginComponent } from './pages/platform-login/platform-login.component';
+import { PartnerReferralResponseComponent } from './pages/referrals/partner-referral-response/partner-referral-response.component';
+import { SpeakingRequestDetailComponent } from './pages/speaking-requests/speaking-request-detail/speaking-request-detail.component';
+import { SpeakingRequestListComponent } from './pages/speaking-requests/speaking-request-list/speaking-request-list.component';
 import { PlatformRoutingModule } from './platform-routing.module';
-import {
-  KosDrawerComponent
-} from './shared/components/kos-drawer/kos-drawer.component';
+import { KosDrawerComponent } from './shared/components/kos-drawer/kos-drawer.component';
 
 @NgModule({
   declarations: [
@@ -64,6 +31,7 @@ import {
     AssignmentDocumentsComponent,
     AssignmentListComponent,
     AssignmentOverviewComponent,
+    AssignmentTravelComponent,
     AssignmentWorkspaceComponent,
     CareNetworkInboxComponent,
     KosDrawerComponent,
